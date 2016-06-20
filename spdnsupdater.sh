d8a194b9759ce5ba8b61b84898a4f01974cb50c2
root <root@spdns.org>
2016-06-20 16:15:39 +0200

    transfer domains to conf file

--- spdnsupdater.sh
+++ spdnsupdater.sh
@@ -11,11 +11,13 @@ fi
 
 
 
+#get current ip
 IP=$(curl -s http://checkip4.spdyn.de/)
 echo "Current IP is $IP"
 
 
 
+#check ip and last check
 if [ "$IP" = "$LASTIP" ];then
 	if [ "$LASTCHECK" -le "$(( $(date +%s) - 86400 ))" ];then
 		echo "force ip update"
@@ -28,20 +30,32 @@ fi
 
 echo "LASTIP=$IP" > /tmp/lastip
 echo "LASTCHECK=$(date +%s)" >> /tmp/lastip
+
+#update string
 updateip(){
 curl --user $1:$2 "https://update.spdyn.de/nic/update?hostname=$1&myip=$IP&pass=$2"
-
-
 }
 
+# load domains and passwords from spdnsupdater.conf
+if [ -f ~/.spdnsupdater.conf ];then
+	echo "import config from ~/.spdnsupdater.conf "
+	source ~/.spdnsupdater.conf 
+else
+	echo "conf file not found"
+	exit
+fi
 
-#enter here your spdns domain and password
-updateip "uxxxxx.spdns.eu" "zmdw-c"
-updateip "dyxxxxx.spdns.eu" "jcgc-g"
-updateip "dm7xxxx.spdns.eu" "cxle-u"
-updateip "qwxxxx.spdns.eu" "kuoj-o"
+# cal end for sequence
+e=$(( $(echo ${DOMAIN[*]} | wc -w) - 1 )) 
 
 
+#perform update
+for (( i=0; i<=$e; i++ ));do
+	echo " "
+	echo "Update domain ${DOMAIN[$i]}"
+	updateip ${DOMAIN[$i]} ${PASSWORD[$i]}
+	echo " "
+done
 
 
 
---
+++ test.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+DOMAIN=(DO1 DO2 DO3)
+PASSWORD=(PW1 PW2 PW3)
+
+e=7
+
+for (( c=1; c<=$e; c++ ))
+do
+   echo "Welcome $c times"
+done
+
+exit
